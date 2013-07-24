ec07b7bb496546f8eb30f2207cce96e355efc182
Kuanting Chen <kuanting17@gmail.com>
2013-07-23 22:32:17 -0700

    add new controller

---
+++ WeatherViewController.h
@@ -0,0 +1,13 @@
+//
+//  WeatherViewController.h
+//  SBACC
+//
+//  Created by Kuanting Chen on 7/23/13.
+//  Copyright (c) 2013 Kuanting Chen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WeatherViewController : UITableViewController
+
+@end
