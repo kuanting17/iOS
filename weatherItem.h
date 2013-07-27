619758928867df9c5b541f1e4a80b14ecc86a60a
Kuanting Chen <kuanting17@gmail.com>
2013-07-27 10:57:03 -0700

    add a weather object class

---
+++ weatherItem.h
@@ -0,0 +1,13 @@
+//
+//  weatherItem.h
+//  SBACC
+//
+//  Created by Kuanting Chen on 7/27/13.
+//  Copyright (c) 2013 Kuanting Chen. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface weatherItem : NSObject
+
+@end
---
+++ weatherItem.m
@@ -0,0 +1,13 @@
+//
+//  weatherItem.m
+//  SBACC
+//
+//  Created by Kuanting Chen on 7/27/13.
+//  Copyright (c) 2013 Kuanting Chen. All rights reserved.
+//
+
+#import "weatherItem.h"
+
+@implementation weatherItem
+
+@end
